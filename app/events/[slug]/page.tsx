85838af9ac43adfd41b95ebcbf239cedc324c39e
Nihal-123-456 <knasn1526@gmail.com>
2026-03-25 09:33:23 +0600

    cached event-details at the component level

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -2,8 +2,6 @@ import EventDetails from "@/components/EventDetails";
 import { cacheLife } from "next/cache";
 
 const Page = async ({ params }: { params: Promise<{ slug: string }> }) => {
-  "use cache";
-  cacheLife("hours");
   const { slug } = await params;
   return <EventDetails slug={slug} />;
 };
--- components/EventDetails.tsx
+++ components/EventDetails.tsx
@@ -4,20 +4,29 @@ import EventBooking from "@/components/EventBooking";
 import { getSimilarEvents } from "@/lib/actions/events.actions";
 import { IEvent } from "@/database";
 import EventCard from "@/components/EventCard";
+import { cacheLife } from "next/cache";
 
 const base_url = process.env.NEXT_PUBLIC_BASE_URL;
 const bookings = 10;
 
-const EventDetailsItems = ({icon, alt, label}:{icon: string, alt: string, label: string}) => {
-  return(
+const EventDetailsItems = ({
+  icon,
+  alt,
+  label,
+}: {
+  icon: string;
+  alt: string;
+  label: string;
+}) => {
+  return (
     <div className="flex-row-gap-2">
-      <Image src={icon} alt={alt} width={17} height={17}/>
+      <Image src={icon} alt={alt} width={17} height={17} />
       <p>{label}</p>
     </div>
-  )
-}
+  );
+};
 
-const EventAgenda = ({agendas}:{agendas:string[]}) => {
+const EventAgenda = ({ agendas }: { agendas: string[] }) => {
   return (
     <div className="agenda">
       <h2>Event Agenda</h2>
@@ -28,39 +37,66 @@ const EventAgenda = ({agendas}:{agendas:string[]}) => {
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-const EventTags = ({tags}:{tags: string[]}) => {
-  return(
+const EventTags = ({ tags }: { tags: string[] }) => {
+  return (
     <div className="flex gap-2 flex-wrap">
       {tags.map((item) => (
-        <div key={item} className="pill">{item}</div>
+        <div key={item} className="pill">
+          {item}
+        </div>
       ))}
     </div>
-  )
-}
+  );
+};
 
-const EventDetails = async ({slug}:{slug:string}) => {
+const EventDetails = async ({ slug }: { slug: string }) => {
+  "use cache";
+  cacheLife("hours");
+
   const response = await fetch(`${base_url}/api/events/${slug}`);
-  const {event} = await response.json();
+  const { event } = await response.json();
 
-  if(!event) { return notFound() }
+  if (!event) {
+    return notFound();
+  }
 
-  const {title, description, overview, image, venue, location, date, time, mode, audience, agenda, organizer, tags} = event;
+  const {
+    title,
+    description,
+    overview,
+    image,
+    venue,
+    location,
+    date,
+    time,
+    mode,
+    audience,
+    agenda,
+    organizer,
+    tags,
+  } = event;
 
-  const similarEvents:IEvent[] = await getSimilarEvents(slug);
+  const similarEvents: IEvent[] = await getSimilarEvents(slug);
 
   return (
     <section id="event">
       <div className="header">
         <h1>Event Description</h1>
-        <p>{description}</p> 
+        <p>{description}</p>
       </div>
 
       <div className="details">
         <div className="content">
-          <Image src={image} alt="Banner Image" width={800} height={800} className="banner"/>
+          <Image
+            src={image}
+            alt="Banner Image"
+            width={800}
+            height={800}
+            className="banner"
+          />
 
           <section className="flex-col-gap-2">
             <h2>Overview</h2>
@@ -70,29 +106,49 @@ const EventDetails = async ({slug}:{slug:string}) => {
           <section className="flex-col-gap-2">
             <h2>Event Details</h2>
 
-            <EventDetailsItems icon="/icons/calendar.svg" alt="date" label={date}/>
-            <EventDetailsItems icon="/icons/clock.svg" alt="time" label={time}/>
-            <EventDetailsItems icon="/icons/pin.svg" alt="location" label={`${location}, ${venue}`}/>
-            <EventDetailsItems icon="/icons/mode.svg" alt="mode" label={mode}/>
-            <EventDetailsItems icon="/icons/audience.svg" alt="audience" label={audience}/>
+            <EventDetailsItems
+              icon="/icons/calendar.svg"
+              alt="date"
+              label={date}
+            />
+            <EventDetailsItems
+              icon="/icons/clock.svg"
+              alt="time"
+              label={time}
+            />
+            <EventDetailsItems
+              icon="/icons/pin.svg"
+              alt="location"
+              label={`${location}, ${venue}`}
+            />
+            <EventDetailsItems icon="/icons/mode.svg" alt="mode" label={mode} />
+            <EventDetailsItems
+              icon="/icons/audience.svg"
+              alt="audience"
+              label={audience}
+            />
           </section>
 
-          <EventAgenda agendas={agenda}/>
+          <EventAgenda agendas={agenda} />
 
           <section className="flex-col-gap-2">
             <h2>About the Organizer</h2>
             <p>{organizer}</p>
           </section>
 
-          <EventTags tags={tags}/>
+          <EventTags tags={tags} />
         </div>
         <aside className="booking">
           <div className="signup-card">
             <h2>Book your spot</h2>
-            {bookings > 0 ? (<p className="text-sm">
-              Join {bookings} people who have already booked their spot
-            </p>): <p className="text-sm">Be the first to book your spot</p>}
-            <EventBooking eventId={event._id}/>
+            {bookings > 0 ? (
+              <p className="text-sm">
+                Join {bookings} people who have already booked their spot
+              </p>
+            ) : (
+              <p className="text-sm">Be the first to book your spot</p>
+            )}
+            <EventBooking eventId={event._id} />
           </div>
         </aside>
       </div>
@@ -100,13 +156,12 @@ const EventDetails = async ({slug}:{slug:string}) => {
       <div className="flex flex-col gap-4 pt-20">
         <h2>Similar Events</h2>
         <div className="events">
-          {similarEvents.length > 0 && similarEvents.map((e:IEvent) => (
-            <EventCard key={e.slug} {...e}/>
-          ))}
+          {similarEvents.length > 0 &&
+            similarEvents.map((e: IEvent) => <EventCard key={e.slug} {...e} />)}
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default EventDetails
+export default EventDetails;
