c3a11338e738f839dba9ac92cbd55b4621e2f9ef
Nihal-123-456 <knasn1526@gmail.com>
2026-03-25 09:40:25 +0600

    added suspense for uncached components

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,9 +1,13 @@
 import EventDetails from "@/components/EventDetails";
-import { cacheLife } from "next/cache";
+import { Suspense } from "react";
 
 const Page = async ({ params }: { params: Promise<{ slug: string }> }) => {
   const { slug } = await params;
-  return <EventDetails slug={slug} />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <EventDetails slug={slug} />;
+    </Suspense>
+  );
 };
 
 export default Page;
