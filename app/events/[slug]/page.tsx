cf79fd760574f0e4a5cb767eb0516252f31c2bbf
Nihal-123-456 <knasn1526@gmail.com>
2026-03-25 09:23:52 +0600

    cached event-details component added

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,8 +1,11 @@
 import EventDetails from "@/components/EventDetails";
+import { cacheLife } from "next/cache";
 
-const Page = async ({params} : {params: Promise<{slug: string}>}) => {
-  const {slug} = await params;
-  return <EventDetails slug={slug}/>
-}
+const Page = async ({ params }: { params: Promise<{ slug: string }> }) => {
+  "use cache";
+  cacheLife("hours");
+  const { slug } = await params;
+  return <EventDetails slug={slug} />;
+};
 
-export default Page
+export default Page;
